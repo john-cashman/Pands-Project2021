aba4e9ee14de42e1d9840a12277ec19ab8462153
john-cashman <johncashmanireland@gmail.com>
2021-03-23 22:21:49 +0000

    The  Sum Median and Mean of Petal Length

--- import.py
+++ import.py
@@ -3,7 +3,33 @@ import pandas as pd
 
 data = pd.read_csv("iris_csv.csv") 
 
+
+
+
+sum = data["sepallength"].sum()
+mean = data["sepallength"].mean()
+median = data["sepallength"].median()
+
+
+print(sum,mean,median)
+
+
+
+
+
+# print(data)
+# print(data["sepallength"].sum())
+# print(sum)
+# print(data["sepallength"].sum())
+# print(data["sepallength"].mean())
+# print(data["sepallength"].median())
+
+
+
+
+
+# print("Sum:",sum_data, "\nMean:", mean_data, "\nMedian:",median_data)
 # print(data.head(11))
 # print(data.sample(20))
 # print(data.columns)
-print(data.shape)
+# print(data.shape)
