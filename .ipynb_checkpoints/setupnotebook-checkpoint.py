826125f161cc104b67d58fe728b4767abef73399
john-cashman <johncashmanireland@gmail.com>
2021-03-24 19:17:10 +0000

    setting up the notebook. Way to messy but will clean it up later

---
+++ .ipynb_checkpoints/setupnotebook-checkpoint.py
@@ -0,0 +1,12 @@
+# import the tools needed to analyse and plot the data. 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+print('done')
+
+# This updates the plotting styles within the program to follow the seaborn style. Not needed,just the seaborn style looks better.
+sns.set()
+
+print('still works')
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Pands-Project
  
+The Irish Flower Data Set is a set of 150 entries of a specific flower.
+
+3 different Iris species:
+Iris Setosa
+Iris Versicolor
+Iris  
+
+sepal is the stem
+petal is the main portion of the flower
