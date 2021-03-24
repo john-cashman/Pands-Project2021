316ce88d48a87b6c8f6e13ec5cad80a2cc70621e
john-cashman <johncashmanireland@gmail.com>
2021-03-24 20:53:41 +0000

    getting the hang of Jupyter notebook and also finding a more efficient way to load the dataset

--- .ipynb_checkpoints/setupnotebook-checkpoint.py
+++ .ipynb_checkpoints/setupnotebook-checkpoint.py
@@ -3,10 +3,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
-
+from sklearn import datasets
 print('done')
 
 # This updates the plotting styles within the program to follow the seaborn style. Not needed,just the seaborn style looks better.
 sns.set()
 
+# loads the Iris dataset from sklearns datasets. Important to include the "()" afterwards. 
+data = datasets.load_iris()
+
+print(data)
+
 print('still works')
+
