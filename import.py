9944c3e4baf3bca709add56a555e3e8fd2fad0c6
john-cashman <johncashmanireland@gmail.com>
2021-03-24 12:23:01 +0000

    Update import.py

--- import.py
+++ import.py
@@ -6,28 +6,27 @@ data = pd.read_csv("iris_csv.csv")
 
 
 
-sum = data["sepallength"].sum()
-mean = data["sepallength"].mean()
-median = data["sepallength"].median()
-
-
-print(sum,mean,median)
 
 
 
+# This describes the data. It gives the count, mean, standard deviation, min,25%,50%, 75% and max.
+print(data.describe()) 
 
 
+
+
+
+
+# sum = data["sepallength"].sum()
+# mean = data["sepallength"].mean()
+# median = data["sepallength"].median()
+# print(sum,mean,median)
 # print(data)
 # print(data["sepallength"].sum())
 # print(sum)
 # print(data["sepallength"].sum())
 # print(data["sepallength"].mean())
 # print(data["sepallength"].median())
-
-
-
-
-
 # print("Sum:",sum_data, "\nMean:", mean_data, "\nMedian:",median_data)
 # print(data.head(11))
 # print(data.sample(20))
