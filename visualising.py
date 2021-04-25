50355958b7809ad981222217b4bdf916370083b5
john-cashman <johncashmanireland@gmail.com>
2021-04-25 18:00:33 +0100

    scatter plot need to add color next

--- visualising.py
+++ visualising.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import seaborn as sns
-
+sns.set()
 from sklearn import datasets
+
 
 data = datasets.load_iris()
 
-iris_df = data
+data["feature_names"]
+
+df = pd.DataFrame(data["data"], columns=data["feature_names"])
+
+df["target"] = data["target"]
+
+df["target_name"] = df["target"].map({0: "setosa", 1: "versicolor", 2: "virginica"})
 
 
 plt.title('Comparison between sepal width and length')
-sns.scatterplot(iris_df['sepal length (cm)'], iris_df['sepal width (cm)']);
-
-print('nope')
+sns.scatterplot(df['sepal length (cm)'], df['sepal width (cm)']);
+plt.show()
